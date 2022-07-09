f33dba3aca282645c251f673950e95c54e19904c
bluff <bluffyx91@gmail.com>
2022-07-09 10:56:26 +0300

    finished the last 2 exercises for this chapter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(number) {
+  let aux = (number-32)*(5/9);
+  return Math.round(aux * 10)/10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(number) {
+  let aux = (number*9/5)+32;
+  return Math.round(aux * 10)/10;
 };
 
 // Do not edit below this line
